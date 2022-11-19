2b44360af6c65875eeff7b4895645d4b146ace36
ExoticTurtles <daniel.garciaea@uanl.edu.mx>
2022-11-18 18:25:55 -0600

    Cambios

--- app.js
+++ app.js
@@ -6,6 +6,12 @@ const mongoose = require('mongoose');
 //const Campground = require('./models/interfaces');
 const ejsMate = require('ejs-mate');
 const catchAsync = require("./utils/catchAsync");
+const { emitWarning } = require('process');
+const User = require('./models/user')
+const bcrypt = require('bcrypt');
+const { Hash } = require('crypto');
+const session = require('express-session')
+
 
 mongoose.connect('mongodb://localhost:27017/TXkennels', {
     useNewUrlParser: true,
@@ -22,25 +28,77 @@ const app = express();
 app.use(express.static(__dirname + '/views'));
 app.engine('ejs', ejsMate)
 app.set('view engine', 'ejs');
+app.use(session({secret: 'notasecret'}))
 app.set('views', path.join(__dirname, 'views'))
 
+app.use(express.urlencoded({extended: true}));
+app.use(methodOverride('_method'));
 
+const requireLogin = (req, res, next) => {
+    if (!req.session.user_id){
+        return res.redirect('/login')
+    }
+    next();
+
+}
 
 app.get('/', (req, res) => {
     res.render('../interfaces/index');
+})
+
+
+app.get('/register',  async (req, res) => {
+    res.render('../interfaces/register');
+})
+
+app.post('/register', async (req,res) => {
+    const {email, username, number, password} = req.body;
+    const hash = await bcrypt.hash(password, 12)
+    const user = new User({
+        email,
+        username, 
+        number,
+        password: hash
+    })
+    await user.save();
+    req.session.user_id = user._id;
+    res.redirect('/')
+
 })
 
 app.get('/login',  async (req, res) => {
     res.render('../interfaces/login');
 })
 
-app.get('/register',  async (req, res) => {
-    res.render('../interfaces/register');
+app.post('/login', async (req,res) => {
+    const {email, password} = req.body;
+    const foundUser = await User.findAndValidate(email, password);
+    if(foundUser){
+        req.session.user_id = foundUser._id;
+        res.redirect('/');
+    }
+    else{
+        res.redirect('/login');
+    }
 })
 
+app.get('/pets',  async (req, res) => {
+    res.render('../interfaces/mascotas');
+})
 
-app.use(express.urlencoded({extended: true}));
-app.use(methodOverride('_method'));
+app.get('/products',  async (req, res) => {
+    res.render('../interfaces/products');
+})
+
+app.post('/logout', (req, res) =>{
+    req.session.user_id = null;
+    res.redirect('/index')
+})
+
+app.get('/secret', requireLogin, (req, res) => {
+    res.send("Secretooooo")
+})
+
 
 app.all('*', (req,res,next) =>{
     next(new ExpressError("Page Not Found", 404))
---
+++ models/user.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const userSchema = new mongoose.Schema({
+    email:{
+        type: String,
+        required: [true, 'El correo electronico no puede ser nulo.']
+    },
+    username:{
+        type: String,
+        required: [true, 'El nombre de usuario no puede ser nulo.']
+    },
+    number:{
+        type: String,
+        required: [true, 'El numero telefonico no puede ser nulo.']        
+    },
+    password:{
+        type: String,
+        required: [true, 'La contraseña no puede ser nulo.']
+    }
+
+})
+
+
+userSchema.statics.findAndValidate = async function(email, password ){
+    const foundUser = await this.findOne({email});
+    const isValid = await bcrypt.compare(password, foundUser.password);
+    return isValid ? foundUser : false;
+
+}
+module.exports = mongoose.model('User', userSchema);
